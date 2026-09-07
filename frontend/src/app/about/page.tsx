67a8773bc8a9bd88dbc1999afdb3689b1b960ad8
Vidit Sharma <vidits852@gmail.com>
2026-09-08 01:38:45 +0530

    add background beams
    removed about page ContainerTestFlip

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -27,7 +27,7 @@ export default function About() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-start justify-center w-full gap-6">
           <h1 className="text-4xl sm:text-5xl font-bold tracking-tight text-zinc-900 dark:text-white">
-            About <ContainerTextFlip words={["BACKSPACES","Mordern"]} textClassName="text-4xl sm:text-5xl font-bold"/>
+            About BackSpaces
           </h1>
 
           <p className="text-lg text-zinc-600 dark:text-zinc-300 leading-relaxed">
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,18 @@
+import { BackgroundBeams } from "@/components/ui/BackgroundBeams";
 import { ContainerTextFlip } from "@/components/ui/ContainerTextFlip";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black min-h-screen">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
+    <div className="relative flex flex-col flex-1 items-center justify-center bg-black font-sans min-h-screen overflow-hidden">
+      <BackgroundBeams />
+
+      <main className="relative z-10 flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16">
         <div className="flex flex-col items-center justify-center w-full">
-          <ContainerTextFlip words={["BACKSPACES", "REVOLUTION"]} textClassName="text-4xl sm:text-7xl font-bold"/>
+          <ContainerTextFlip words={["BACKSPACES", "REVOLUTION"]} />
+          <button className="shadow-[inset_0_0_0_2px_#616467] text-black text-xs my-5 px-4 py-2 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
+            Dig In
+          </button>
         </div>
       </main>
     </div>
