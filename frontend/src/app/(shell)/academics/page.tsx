dfbcd5a38d511035ea760c08fca81db012d43f3a
Vidit Sharma <vidits852@gmail.com>
2026-09-25 16:57:04 +0530

    add pages

---
+++ frontend/src/app/(shell)/academics/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AcademicsPage = () => {
+    return (
+        <div>
+            <h1>
+                here will be your academic information
+            </h1>
+        </div>
+    )
+}
+
+export default AcademicsPage
